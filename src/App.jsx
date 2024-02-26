ec2c7d8dc775b07580d3369a924a2ababa6c123c
alexmf91 <alexmunoz91@gmail.com>
2024-02-26 19:52:47 +0100

    Añadir configuracion wagmi y connectkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { ConnectKitProvider } from 'connectkit'
+import { WagmiConfig } from 'wagmi'
 import { AppLayout } from './components/ui/layouts'
+import { config } from './config/wagmi'
 import { Home } from './pages'
 
 function App() {
   return (
-    <AppLayout>
-      <Home />
-    </AppLayout>
+    <WagmiConfig config={config}>
+      <ConnectKitProvider mode="light">
+        <AppLayout>
+          <Home />
+        </AppLayout>
+      </ConnectKitProvider>
+    </WagmiConfig>
   )
 }
 
