03f57204200c3dc336ee1e582b18047a2510b4ef
alexmf91 <alexmunoz91@gmail.com>
2024-02-26 19:53:08 +0100

    Añadir boton conexion wallet de connectkit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { ConnectKitButton } from 'connectkit'
 export default function Header() {
   return (
     <header className="border-b shadow py-2 px-3 flex justify-between items-center">
@@ -5,7 +6,7 @@ export default function Header() {
       <img src="/blockmaker-small-logo.png" alt="blockmaker-mobile-logo" className="sm:hidden" width={47} />
       {/* Logo para version desktop */}
       <img src="/blockmaker-full-logo.png" alt="blockmaker-desktop-logo" className="hidden sm:block" width={300} />
-      <button className="bg-gray-100 rounded-xl px-2 text-sm h-fit py-2">Connect Wallet</button>
+      <ConnectKitButton showBalance />
     </header>
   )
 }
